e3654057ae0cc9b133d6704634448cbd9faa53c3
237370 Florczak Wojciech <237370@edu.p.lodz.pl>
2023-10-22 13:07:22 +0200

    BacktracingSudokuSolverTest upgrade

--- sudoku/src/test/java/pl/sudoku/BacktrackingSudokuSolverTest.java
+++ sudoku/src/test/java/pl/sudoku/BacktrackingSudokuSolverTest.java
@@ -7,6 +7,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class BacktrackingSudokuSolverTest {
 
     @Test
+    void solveTest() {
+        BacktrackingSudokuSolver backtrackingSudokuSolver = new BacktrackingSudokuSolver();
+        SudokuBoard sudokuBoard = new SudokuBoard(backtrackingSudokuSolver);
+        sudokuBoard.solveGame();
+
+        int[][] testBoard = new int[9][9];
+        sudokuBoard.getBoard(testBoard);
+
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+
+                for (int i = row + 1; i < 9; i++) {
+                    if (testBoard[row][column] == testBoard[i][column]) {
+                        fail();
+                    }
+                }
+
+                for (int i = column + 1; i < 9; i++) {
+                    if (testBoard[row][column] == testBoard[row][i]) {
+                        fail();
+                    }
+                }
+
+                int boxStartRow = row - row % 3;
+                int boxStartColumn = column - column % 3;
+
+                for (int i = boxStartRow; i < boxStartRow + 3; i++) {
+                    for (int j = boxStartColumn; j < boxStartColumn + 3; j++) {
+                        if (testBoard[row][column] == testBoard[i][j] && (i != row && j != column)) {
+                            fail();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
     void solveComparisonTest() {
         int[][] testBoard1 = new int[9][9];
         int[][] testBoard2 = new int[9][9];
