94efedfe44c2aadb8d1311b848b08c4355bd9319
237370 Florczak Wojciech <237370@edu.p.lodz.pl>
2023-10-24 14:24:31 +0200

    Upgrading SudokuField Class

--- sudoku/src/main/java/pl/sudoku/SudokuField.java
+++ sudoku/src/main/java/pl/sudoku/SudokuField.java
@@ -3,11 +3,19 @@ package pl.sudoku;
 public class SudokuField {
     private int value;
 
+    public SudokuField(int value) {
+        if (value >= 1 && value <= 9) {
+            this.value = value;
+        }
+    }
+
     public int getFieldValue() {
         return value;
     }
 
-    public void setFieldValue(int x) {
-        value = x;
+    public void setFieldValue(int newValue) {
+        if (newValue >= 1 && newValue <= 9) {
+            value = newValue;
+        }
     }
 }
