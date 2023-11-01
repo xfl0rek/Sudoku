7794659713bd02964605632cf6b4118f159d1edf
247765 Poddemski Jakub <247765@edu.p.lodz.pl>
2023-11-01 18:56:39 +0100

    Klasa abstrakcyjna z metoda verify

---
+++ sudoku/src/main/java/pl/sudoku/SudokuVerify.java
@@ -0,0 +1,21 @@
+package pl.sudoku;
+
+public abstract class SudokuVerify {
+
+    protected SudokuField[] sudokuFields;
+
+    public SudokuVerify(SudokuField[] sudokuFields){
+        this.sudokuFields = sudokuFields;
+    }
+
+    public boolean verify(){
+        for(int w = 0; w < 9; w++){
+            for(int k = w + 1; k < 9; k++){
+                if(sudokuFields[w].getFieldValue() == sudokuFields[k].getFieldValue()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
