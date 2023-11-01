3d3a814222cf515a8ef6b40dab879b6eb61bf153
247765 Poddemski Jakub <247765@edu.p.lodz.pl>
2023-11-01 21:07:21 +0100

    SudokuFieldTest

---
+++ sudoku/src/test/java/pl/sudoku/SudokuFieldTest.java
@@ -0,0 +1,23 @@
+package pl.sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SudokuFieldTest {
+
+    @Test
+    public void SetterGetterTest(){
+        SudokuField sudokuField = new SudokuField();
+        sudokuField.setFieldValue(1);
+        assertEquals(sudokuField.getFieldValue(), 1);
+
+        sudokuField.setFieldValue(10);
+        assertEquals(sudokuField.getFieldValue(), 1);
+
+        sudokuField.setFieldValue(-1);
+        assertEquals(sudokuField.getFieldValue(), 1);
+
+    }
+
+}
