819f659d8f08db7da5da60ee0f335feac301c44a
237370 Florczak Wojciech <237370@edu.p.lodz.pl>
2023-11-23 11:43:08 +0100

    FileSudokuBoardDao tests

---
+++ sudoku/src/test/java/pl/sudoku/FileSudokuBoardDaoTest.java
@@ -0,0 +1,38 @@
+package pl.sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSudokuBoardDaoTest {
+
+    boolean readAndWriteTestHelper(SudokuBoard sudokuBoard1, SudokuBoard sudokuBoard2) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sudokuBoard1.getValue(i, j) != sudokuBoard2.getValue(i, j)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Test
+    public void readAndWriteTest() {
+        BacktrackingSudokuSolver backtrackingSudokuSolver = new BacktrackingSudokuSolver();
+        SudokuBoard sudokuBoard1 = new SudokuBoard(backtrackingSudokuSolver);
+        sudokuBoard1.solveGame();
+        SudokuBoardDaoFactory sudokuBoardDaoFactory = new SudokuBoardDaoFactory();
+        SudokuBoard sudokuBoard2;
+
+        Dao<SudokuBoard> sudokuBoardDao = sudokuBoardDaoFactory.getFileDao("testFile");
+        sudokuBoardDao.write(sudokuBoard1);
+
+        sudokuBoard2 = sudokuBoardDao.read();
+
+        assertNotNull(sudokuBoard2);
+
+        assertTrue(readAndWriteTestHelper(sudokuBoard1, sudokuBoard2));
+    }
+}
