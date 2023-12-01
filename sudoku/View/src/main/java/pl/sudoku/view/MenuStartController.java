0aa216e3efc5967759dc882ff583916b1c3b18eb
237370 Florczak Wojciech <237370@edu.p.lodz.pl>
2023-12-01 20:04:37 +0100

    added easyGame method

--- sudoku/View/src/main/java/pl/sudoku/view/MenuStartController.java
+++ sudoku/View/src/main/java/pl/sudoku/view/MenuStartController.java
@@ -1,12 +1,22 @@
 package pl.sudoku.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import pl.sudoku.GameLevel;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MenuStartController {
 
+    private Stage window;
+    private Parent root;
     private static GameLevel level;
+
     @FXML
     Button easyButton = new Button("Easy");
 
@@ -15,4 +25,11 @@ public class MenuStartController {
 
     @FXML
     Button hardButton = new Button("Hard");
+
+    public void easyGame() throws IOException {
+        level = GameLevel.Easy;
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("board-view.fxml")));
+        window = (Stage) easyButton.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
 }
