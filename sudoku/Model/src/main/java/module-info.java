704009cf05e13f31dbbc7e75195c33b9d892db9a
237370 Florczak Wojciech <237370@edu.p.lodz.pl>
2024-01-13 12:06:13 +0100

    added createConnection method and constructor for JdbcSudokuBoardDao class

--- sudoku/Model/src/main/java/module-info.java
+++ sudoku/Model/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ModelProject {
     requires org.apache.commons.lang3;
     requires log4j;
     requires slf4j.api;
+    requires java.sql;
 
     exports pl.sudoku;
     exports pl.sudoku.exceptions;
--- sudoku/Model/src/main/java/pl/sudoku/JdbcSudokuBoardDao.java
+++ sudoku/Model/src/main/java/pl/sudoku/JdbcSudokuBoardDao.java
@@ -1,6 +1,31 @@
 package pl.sudoku;
 
+import pl.sudoku.exceptions.DaoException;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
 public class JdbcSudokuBoardDao implements Dao<SudokuBoard> {
+
+    private Connection connection;
+    private String url = "jdbc:postgresql://localhost:5432/sudokuDB";
+    private String username = "postgres";
+    private String password = "name";
+    private String fileName;
+
+    JdbcSudokuBoardDao(String fileName) throws DaoException {
+        this.fileName = fileName;
+        createConnection();
+    }
+
+    private void createConnection() {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException exception) {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("Lang");
+            throw new DaoException(resourceBundle.getString("connectionException"), exception);
+        }
+    }
 
     @Override
     public SudokuBoard read() {
